ed796b399dfd4d451b0adb9d5733424a801cba6e
so123789 <souparnikac09@gmail.com>
2026-04-26 22:34:26 +0530

    Initial commit: Task Manager

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { ThemeProvider } from './context/ThemeContext'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
+import Dashboard from './Pages/Dashboard'
 
 function App() {
+  const [token, setToken] = useState(localStorage.getItem('token'))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={token ? <Navigate to="/dashboard" /> : <Login setToken={setToken} />} />
+          <Route path="/register" element={token ? <Navigate to="/dashboard" /> : <Register setToken={setToken} />} />
+          <Route path="/dashboard" element={token ? <Dashboard setToken={setToken} /> : <Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
